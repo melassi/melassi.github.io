e7ef5bc345500a999ecc6d98d5113ccbf6847e58
melassi <33429302+melassi@users.noreply.github.com>
2018-01-14 13:04:00 +0100

    night mode?

--- slides/js/index.js
+++ slides/js/index.js
@@ -1,3 +1,23 @@
+var d930 = new Date(2010, 12, 21, 9, 30, 0, 0), // today 9:30:00:000
+    d931 = new Date(2010, 12, 21, 9, 31, 0, 0), // today 9:31:00:000
+    t930 = d930.getTime(),
+    t931 = d931.getTime();
+
+
+console.log(t931 > t930);
+This way your code can check against a static 9:30 time.
+
+var time930 = new Date(2010, 12, 21, 9, 30, 0, 0).getTime(),
+    now = new Date();
+
+if(now.getTime() >= time930){
+  
+document.querySelector('[data-switch-contrast]').addEventListener('click', function() {
+  document.body.classList.toggle('nightmode');
+});
+
+}
+
 function cancelFullScreen() {
     if (document.cancelFullScreen) {
         document.cancelFullScreen();
